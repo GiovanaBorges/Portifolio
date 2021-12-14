f73a82abb45c6f1a3ab54a95be987c8542cef0d0
giovana borges <gigi12345as17@gmail.com>
2021-12-14 20:01:48 -0300

    upgrade themeSwitcher

--- src/components/ThemeSwitcher/ThemeSwitcher.style.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.style.jsx
@@ -1,7 +1,6 @@
+import styled from "styled-components";
 import { Sun } from "@styled-icons/bootstrap/Sun";
 import { Moon } from "@styled-icons/bootstrap/Moon";
-import styled from "styled-components";
 
 export const ButtonSun = styled(Sun)``;
-
 export const ButtonNight = styled(Moon)``;
