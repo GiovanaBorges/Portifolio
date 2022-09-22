48579576898aee3e277aa62b2c930cb59322f07b
GiovanaBorges <gigi12345as17@gmail.com>
2022-09-21 21:33:19 -0300

    :feat add route system

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,23 @@
-import {BrowserRouter,Route} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Navbar from "./components/navbar";
 import Layout from "./pages/Layout"
+import Project from "./pages/Project";
+import Skills from "./pages/Skills";
+import Sobre from "./pages/Sobre"
 
-export const Routes = () =>{
+export const RoutesApp = ({theme,toggleTheme}) =>{
     return(
-        <BrowserRouter>
-            <Route component = { Layout }  path="/" exact/>
-        </BrowserRouter>
+    <>
+        <Router>
+        <Navbar toggleTheme={toggleTheme} theme={theme} />    
+          <Routes>
+          
+            <Route element={<Layout/>}  path="/" exact />
+            <Route element={<Sobre/>}  path="/sobre"  />
+            <Route element={<Project/>}  path="/projects"/>
+            <Route element={<Skills/>}  path="/skill"/>
+          </Routes>
+        </Router>
+        </>
     )
 }
